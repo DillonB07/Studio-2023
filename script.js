a4b2ff3fe313c7c079d4ec8a3e8e2ed1982f3423
Dillon Barnes <83948303+DillonB07@users.noreply.github.com>
2023-01-21 18:48:49 +0000

    Final changes - Responsiveness, polish

--- script.js
+++ script.js
@@ -9,19 +9,13 @@ document.getElementById("cards").onmousemove = e => {
     };
 }
 
+document.getElementById("contact-cards").onmousemove = e => {
+    for (const card of document.getElementsByClassName("contact-icon")) {
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
 
-export function showcaseClick() {
-    window.open('https://replit-showcase.dillonb07.repl.co', '_blank')
-}
-
-export function filesClick() {
-    window.open('https://replfiles.dillonb07.studio', '_blank')
-}
-
-export function googleClick() {
-    window.open('https://google.dillonb07.studio', '_blank')
-}
-
-export function dizzleClick() {
-    window.open('https://dizzle.dillonb07.repl.co', '_blank')
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+    };
 }
