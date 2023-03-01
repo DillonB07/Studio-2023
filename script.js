19dc925bec4309392b7a2e45903951d7d5c3e960
Dillon Barnes <dillonb07dev@gmail.com>
2023-03-01 19:04:26 +0000

    Add older projects

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-document.getElementById("cards").onmousemove = e => {
+document.getElementById("new-cards").onmousemove = e => {
+    for (const card of document.getElementsByClassName("card")) {
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+        card.style.setProperty("---mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+    };
+}
+
+document.getElementById("old-cards").onmousemove = e => {
     for (const card of document.getElementsByClassName("card")) {
         const rect = card.getBoundingClientRect(),
             x = e.clientX - rect.left,
