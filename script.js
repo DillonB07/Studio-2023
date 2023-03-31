7182dad2309f5dff7649c1ca96fba0a0a12879f4
DillonB07 <dillonbarnes07@gmail.com>
2023-03-31 17:32:08 +0100

    Add arrow to all sections

--- script.js
+++ script.js
@@ -4,12 +4,24 @@ document.addEventListener("DOMContentLoaded", () => {
   container.addEventListener("scroll", (event) => {
     const introSection = document.getElementById("intro");
     const arrow = document.getElementById("scroll-arrow");
+    const link = document.getElementById("scroll-link");
 
-    const rect = introSection.getBoundingClientRect();
-    if (rect.top < 0) {
-      arrow.style.display = "none";
+    let rect = introSection.getBoundingClientRect();
+
+    if (rect.top == 0) {
+      link.setAttribute("href", "#new-projects");
+      arrow.style.display = "inline-block";
+    } else if (rect.top == -1018) {
+      link.setAttribute("href", "#old-projects");
+      arrow.style.display = "inline-block";
+    } else if (rect.top == -2036) {
+      link.setAttribute("href", "#about");
+      arrow.style.display = "inline-block";
+    } else if (rect.top == -3054) {
+      link.setAttribute("href", "#contact");
+      arrow.style.display = "inline-block";
     } else {
-      arrow.style.display = "block";
+      arrow.style.display = "none";
     }
   });
 
@@ -35,16 +47,18 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
-  document.querySelector(".contact-cards").addEventListener("mousemove", (e) => {
-    document.querySelectorAll(".contact-icon").forEach((card) => {
-      const rect = card.getBoundingClientRect(),
-        x = e.clientX - rect.left,
-        y = e.clientY - rect.top;
+  document
+    .querySelector(".contact-cards")
+    .addEventListener("mousemove", (e) => {
+      document.querySelectorAll(".contact-icon").forEach((card) => {
+        const rect = card.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
 
-      card.style.setProperty("---mouse-x", `${x}px`);
-      card.style.setProperty("--mouse-y", `${y}px`);
+        card.style.setProperty("---mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+      });
     });
-  });
 
   // Add a event listener to detect when the user isn't on the intro section and hide the arrow image
 });
