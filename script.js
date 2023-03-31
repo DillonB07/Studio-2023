6efb9eba97ef315d1a1140ea981aa9d0e0ca30a9
DillonB07 <dillonbarnes07@gmail.com>
2023-03-31 17:38:08 +0100

    Improve arrow logic

--- script.js
+++ script.js
@@ -8,21 +8,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let rect = introSection.getBoundingClientRect();
 
-    if (rect.top == 0) {
-      link.setAttribute("href", "#new-projects");
-      arrow.style.display = "inline-block";
-    } else if (rect.top == -1018) {
-      link.setAttribute("href", "#old-projects");
-      arrow.style.display = "inline-block";
-    } else if (rect.top == -2036) {
-      link.setAttribute("href", "#about");
-      arrow.style.display = "inline-block";
-    } else if (rect.top == -3054) {
+     if (rect.top == -4072) {
+      arrow.style.display = "none";
+    } else if (rect.top <= -3054) {
       link.setAttribute("href", "#contact");
       arrow.style.display = "inline-block";
-    } else {
-      arrow.style.display = "none";
-    }
+    } else if (rect.top <= -2036) {
+      link.setAttribute("href", "#about");
+      arrow.style.display = "inline-block";
+    } else if (rect.top <= -1018) {
+      link.setAttribute("href", "#old-projects");
+      arrow.style.display = "inline-block";
+    } else if (rect.top <= 0) {
+      link.setAttribute("href", "#new-projects");
+      arrow.style.display = "inline-block";
+    } 
+
   });
 
   document.querySelector(".new-cards").addEventListener("mousemove", (e) => {
